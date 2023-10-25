dcc62a3922e906cfe4adfc3527770fe87578f593
Raissa Assis <raissaassis017@gmail.com>
2023-10-25 15:45:12 -0300

    create user with crypto pass

--- src/controllers/user/postUser.js
+++ src/controllers/user/postUser.js
@@ -3,16 +3,23 @@ import user from '../../models/userModel.js'
 const postUser = async (req, res, next) => {
   try {
     const userData = req.body
-    const [result] = await user.create(userData)
-    if (result.affectedRows === 1) {
-      res.json({
-        success: "Usuário inserido com Sucesso!",
-        user: {
-          id: result.insertId,
-          ...userData
-        }
+    bcrypt.hash(userData.pass, 10, async (error, hash) => {
+      if (error) return res.status(500).json({
+        error: "Erro ao gerar senha criptografada!",
       })
-    }
+      userData.pass = hash
+      const [result] = await user.create(userData)
+      delete userData.pass
+      if (result.affectedRows === 1) {
+        res.json({
+          success: "Usuário inserido com Sucesso!",
+          user: {
+            id: result.insertId,
+            ...userData
+          }
+        })
+      }
+    })
   } catch (error) {
     console.log(error)
     res.status(500).json({
