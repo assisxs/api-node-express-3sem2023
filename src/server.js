7f89977023281a081f3c962a735b74ebe3ae9204
Raissa Assis <raissaassis017@gmail.com>
2023-11-06 16:42:44 -0300

    create session login and logout user

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 import express from "express";
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
+import cookieParser from 'cookie-parser'
 import userRouter from './routers/userRouter.js'
 import productRouter from './routers/productRouter.js'
 import authRouter from './routers/authRouter.js'
@@ -10,20 +10,21 @@ import { PORT } from "./config.js";
 
 const api = express();
 
+var corsOptions = {
+  credentials: true,
+  origin: 'http://127.0.0.1:5173'
+}
+//converte toda requisição com body json para objeto no req.body
+api.use(cors(corsOptions))
+api.use(bodyParser.json())
+api.use(cookieParser())
 
-api.use(cors())
-api.use(bodyParser.json()) // Converte toda requisição com body json para objeto no req.body
 
-
-api.get("/", (req, res, next) => {
-  res.json({ message: "Bem Vindo a nossa API!" });
-});
-
+api.get('/', (req, res) => {
+    res.json({message: "Bem-vindo a nossa API"})
+})
 api.use('/user', userRouter)
-api.use('/product', productRouter)
 api.use('/auth', authRouter)
-
-
 api.listen(PORT, () => {
-  console.log(`server running at ${PORT}!`);
-});
+    console.log(`Servidor rodando na porta ${PORT}! http://localhost:${PORT}`)
+})
